587ea6be6c36806f730465daa0a1496a166e0777
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-03-19 17:41:05 +0200

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,27 +2,39 @@ import React from 'react'
 import "./header.scss"
 import { Link } from 'react-scroll'
 import Logo from './Logo'
+import { useTranslation } from 'react-i18next'
 
 const Header = () => {
+
+    const { t, i18n } = useTranslation()
+
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang)
+    } 
+
   return (
     <header className='header'>
         <div className="container">
             <Logo/>
-            <nav>
+            <nav className='nav'>
                 <ul className="menu">
                     <li className="item">
-                        <Link to=''>About</Link>
+                        <Link to='about' smooth={true} duration={500}>{t("navigation.about")}</Link>
                     </li>
                     <li className="item">
-                        <Link to=''>Skills</Link>
+                        <Link to='skills'>{t("navigation.skills")}</Link>
                     </li>
                     <li className="item">
-                        <Link to=''>Portfolio</Link>
+                        <Link to='portfolio'>{t("navigation.portfolio")}</Link>
                     </li>
                     <li className="item">
-                        <Link to=''>Contact Me</Link>
+                        <Link to='contact'>{t("navigation.contact")}</Link>
                     </li>
                 </ul>
+                <div className="language-switcher">
+                    <button onClick={() => changeLanguage("en")}>en</button>
+                    <button onClick={() => changeLanguage("pl")}>pl</button>
+                </div>
             </nav>
         </div>
     </header>
