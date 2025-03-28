ae0dee5b9d3cdb62add27e19f40596bdf23172f2
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-03-29 00:29:15 +0200

    commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,18 +3,19 @@ import { IoIosAt } from "react-icons/io";
 import { GrGithub } from "react-icons/gr";
 import { TbBrandLinkedinFilled } from "react-icons/tb";
 import "./home.scss"
+import homeImage from "../../assets/img/HomeImage.png";
 
 const Home = () => {
     return (
         <section className='home-section'>
             <div className="home-wrapper">
                 <div className="home-left"></div>
-                <div className="home-right"></div>
+                <div className="home-right"> <img src={homeImage} alt="" className='home-img'/></div>
                 <div className="home-content">
                     <div className="container">
                         <h2 className='title'>Hi, I am</h2>
-                        <h1 className='name-title'>Tomas Gajda</h1>
-                        <span className='subTitle'>Front-end Developer / UI Designer</span>
+                        <h1 className='name-title'>Erik Gorodyskyy</h1>
+                        <span className='subTitle'>Front-end and React Developer</span>
                     </div>
                 </div>
             </div>
