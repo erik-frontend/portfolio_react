0b4e53d723c55554d6b6fe49fc0e9fad9850b770
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-04-04 23:38:20 +0300

    commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -39,6 +39,11 @@ const Home = ({sticky}) => {
                     </div>
                 </div>
             </div>
+            <div className="info">
+                <h1 className="info-title">IT BERRIES</h1>
+                <p className="info-text">Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est. Morbi commodo, eros in dignissim tempus, lacus odio rutrum augue, in semper sem magna quis tellus. Etiam enim erat, suscipit eu semper a, dictum sit amet elit. Nunc egestas nisi eget enim gravida facilisis. Pellentesque laoreet varius turpis vel pharetra. Ut ante justo, consequat vitae elementum tempor, accumsan nec eros. </p>
+                <button className="info-btn">READ MORE</button>
+            </div>
         </section>
     )
 }
