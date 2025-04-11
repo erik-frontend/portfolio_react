2dd76fc90c349d0057b2ae188e70c5284f1ff2af
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-04-11 23:57:27 +0300

    commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,13 +3,14 @@ import SectionTitle from '../../utils/SectionTitle'
 import Explore from '../../utils/Explore'
 import Separation from '../../utils/Separation'
 import { services } from '../../utils/dataSource'
+import "./about.scss"
 
 const About = () => {
   return (
     <section className='about'>
       <div className="container">
         <SectionTitle title="about me"/>
-        <p className='about-subtitle'></p>
+        <p className='about-subtitle'>Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est.</p>
         <Explore text="explore"/>
         <Separation/>
         <div className="about-services">
