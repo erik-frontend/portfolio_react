1d2f6c6bc638fa8691e0f475bc8d538b6bcefcab
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-05-10 10:09:34 +0300

    commit
    portfolio section

--- src/components/portfolio/FilterBar.jsx
+++ src/components/portfolio/FilterBar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { catigories } from '../../utils/dataSource'
 
 const FilterBar = () => {
+
+const [active, setIsActive] = useState(catigories[0]?.key ?? '')
+// console.log(active);
+
+  const handleActive = (key) => {
+    setIsActive(key)
+  }
+
   return (
     <div className='filter'>
         {catigories.map(item => (
-            <button className='filter-btn' key={item.key}>{item.label}</button>
+            <button 
+            className={`filter-btn ${active === item.key ? "active" : ""}`} 
+            onClick={() => handleActive(item.key)}
+            key={item.key}>{item.label}</button>
         ))}
     </div>
   )
